7769f5639f157ffd97f6e91b69354ff292125375
och6z <44880194+och6z@users.noreply.github.com>
2021-08-10 18:56:17 +0200

    Update & add ExampleIfTrue & TestIfTrue & IfTrue functions

--- cstructs/if.go
+++ cstructs/if.go
@@ -1,33 +1,9 @@
 package cstructs
 
-func HelloIf() string {
-	name := ""
-	if name == "" {
-		name = "World!"
+func IfTrue() int {
+	count := 0
+	if true {
+		count = count + 1
 	}
-	return HelloWorldPrefix + name
-}
-
-// func HelloIfInitialization() string {
-// 	if name := "World!"; name != "" {
-// 		return HelloWorldPrefix + name
-// 	}
-// }
-func HelloIfElse() string {
-	name := "World!"
-	if name == "" {
-		return HelloWorldPrefix + ""
-	} else {
-		return HelloWorldPrefix + name
-	}
-}
-func HelloIfElseIf() string {
-	name := "World!"
-	if name == "" {
-		return HelloWorldPrefix + ""
-	} else if name == "World!" {
-		return HelloWorldPrefix + name
-	} else {
-		return HelloWorldPrefix + "Golang!"
-	}
+	return count
 }
--- cstructs/if_test.go
+++ cstructs/if_test.go
@@ -7,79 +7,17 @@ import (
 	"example.com/m/cstructs"
 )
 
-// "If" statements specify the conditional execution of two branches
-// according to the value of a boolean expression.
-func Example_ifStatement() {
-	var name = ""
-	if name == "" {
-		name = "World!"
+func ExampleIfTrue() {
+	count := 0
+	if true {
+		count = count + 1
 	}
-	fmt.Println(HelloWorldPrefix + name)
-	// Output: Hello, World!
+	fmt.Println(count)
+	// output: 1
 }
-func TestHelloIf(t *testing.T) {
-	got := cstructs.HelloIf()
-	want := "Hello, World!"
+func TestIfTrue(t *testing.T) {
+	got := cstructs.IfTrue()
+	want := 1
 
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-}
-
-// The expression may be preceded by a simple statement,
-// which executes before the expression is evaluated.
-func Example_ifInitializationStatement() {
-	if name := "World!"; name != "" {
-		fmt.Println(HelloWorldPrefix + name)
-	}
-	// Output: Hello, World!
-}
-
-// func TestHelloIfInitialization(t *testing.T) {
-// 	got := cstructs.HelloInitialization()
-// 	want := "Hello, World!"
-
-// 	if got != want {
-// 		t.Errorf("got %q want %q", got, want)
-// 	}
-// }
-
-// If the expression evaluates to true, the "if" branch is executed,
-// otherwise, if present, the "else" branch is executed.
-func Example_ifElseStatement() {
-	if name := "World!"; name == "" {
-		fmt.Println(HelloWorldPrefix + "")
-	} else {
-		fmt.Println(HelloWorldPrefix + name)
-	}
-	// Output: Hello, World!
-}
-func TestHelloIfElse(t *testing.T) {
-	got := cstructs.HelloIfElse()
-	want := "Hello, World!"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
-}
-
-// The else if statement will be placed between the
-// if statement and the else statement.
-func Example_ifElseIfStatement() {
-	if name := "World!"; name == "" {
-		fmt.Println(HelloWorldPrefix + "")
-	} else if name == "World!" {
-		fmt.Println(HelloWorldPrefix + name)
-	} else {
-		fmt.Println(HelloWorldPrefix + "Golang!")
-	}
-	// Output: Hello, World!
-}
-func TestHelloIfElseIf(t *testing.T) {
-	got := cstructs.HelloIfElseIf()
-	want := "Hello, World!"
-
-	if got != want {
-		t.Errorf("got %q want %q", got, want)
-	}
+	assertCorrectMessageInt(got, want, t)
 }
